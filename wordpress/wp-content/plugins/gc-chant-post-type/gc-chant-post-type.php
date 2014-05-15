832987b87250e3f2c70fc34f1d43b3cc9aaee250
nwshane <django09@gmail.com>
2014-05-15 08:25:11 +0400

    gc chant post type now saves to database along with georgian text metabox. georgian text metabox has unresolved issue: character spaces are deleted when it is saved

--- wordpress/wp-content/plugins/gc-chant-post-type/gc-chant-post-type.php
+++ wordpress/wp-content/plugins/gc-chant-post-type/gc-chant-post-type.php
@@ -67,8 +67,52 @@ function gc_chant_add_post_meta_boxes() {
     );
 }
 
+/*
+ * Save the meta box's post metadata.
+ * Code copied and modified from http://www.smashingmagazine.com/2011/10/04/create-custom-post-meta-boxes-wordpress/
+ */
+function gc_chant_save_post_meta( $post_id, $post ) {
+
+    /* Verify the nonce before proceeding. */
+    if ( !isset( $_POST['georgian_text_meta_box_nonce'] ) || !wp_verify_nonce( $_POST['georgian_text_meta_box_nonce'], basename( __FILE__ ) ) )
+        return $post_id;
+
+    /* Get the post type object. */
+    $post_type = get_post_type_object( $post->post_type );
+
+    /* Check if the current user has permission to edit the post. */
+    if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
+        return $post_id;
+
+    /* Get the posted data and sanitize it for use as an HTML class. */
+    $new_meta_value = ( isset( $_POST['georgian-text-meta-box'] ) ? sanitize_html_class( $_POST['georgian-text-meta-box'] ) : '' );
+
+    /* Get the meta key. */
+    $meta_key = 'georgian-text-meta-box';
+
+    /* Get the meta value of the custom field key. */
+    $meta_value = get_post_meta( $post_id, $meta_key, true );
+
+    /* If a new meta value was added and there was no previous value, add it. */
+    if ( $new_meta_value && '' == $meta_value )
+        add_post_meta( $post_id, $meta_key, $new_meta_value, true );
+
+    /* If the new meta value does not match the old value, update it. */
+    elseif ( $new_meta_value && $new_meta_value != $meta_value )
+        update_post_meta( $post_id, $meta_key, $new_meta_value );
+
+    /* If there is no new meta value but an old value exists, delete it. */
+    elseif ( '' == $new_meta_value && $meta_value )
+        delete_post_meta( $post_id, $meta_key, $meta_value );
+}
+
+/**
+ * Sets up meta boxes
+ */
 function gc_chant_post_meta_boxes_setup() {
     add_action( 'add_meta_boxes', 'gc_chant_add_post_meta_boxes' );
+
+    add_action( 'save_post', 'gc_chant_save_post_meta', 10, 2);
 }
 
 add_action( 'load-post.php', 'gc_chant_post_meta_boxes_setup' );

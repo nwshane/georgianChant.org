a940db2e7a5ebc949d537e4aa766eb835b36a9e2
nwshane <django09@gmail.com>
2014-07-03 09:22:45 +0400

    Added chant variants meta box to chant editor and within it a dropdown for monastery tradition.

--- wordpress/wp-content/plugins/gc-chant-post-type/gc-chant-post-type.php
+++ wordpress/wp-content/plugins/gc-chant-post-type/gc-chant-post-type.php
@@ -219,6 +219,32 @@ function chant_liturgy_culture_meta_box_callback( $chant ) { ?>
 
 <?php }
 
+function chant_variants_meta_box_callback( $chant ) { ?>
+
+    <p>If the chant variant comes out of the oral tradition...</p>
+
+    <?php
+    wp_nonce_field( basename( __FILE__ ), 'monastery_tradition_nonce' );
+    $monastery_tradition = esc_attr(get_post_meta( $chant->ID, 'monastery-tradition', true));
+    ?>
+
+    <div>
+        <label for="monastery-tradition"><b><?php _e( 'Monastery Tradition', 'example' )?></b> - <?php _e( 'Enter the monastery tradition of the chant.', 'example' ); ?></label>
+        <br>
+        <select id="monastery-tradition" name="monastery-tradition">
+            <option value></option>
+
+            <?php
+            $possible_monastery_traditions = [ "Gelati", "Shemokmedi", "East Georgia" ];
+            foreach ( $possible_monastery_traditions as $possible_monastery_tradition ) { ?>
+                <option value="<?php echo $possible_monastery_tradition; ?>" <?php if ( $monastery_tradition === $possible_monastery_tradition ) { ?>selected<?php } ?>><?php echo $possible_monastery_tradition; ?></option>
+            <?php } ?>
+
+        </select>
+    </div>
+
+<?php }
+
 function gc_chant_add_post_meta_boxes() {
     add_meta_box(
         'chant-rubric-meta-box',
@@ -251,6 +277,15 @@ function gc_chant_add_post_meta_boxes() {
         'chant-liturgy-culture-meta-box',
         esc_html__( 'Liturgical and Cultural Role', 'example' ),
         'chant_liturgy_culture_meta_box_callback',
+        'gc_chant',
+        'normal',
+        'default'
+    );
+
+    add_meta_box(
+        'chant-variants-meta-box',
+        esc_html__( 'Variants', 'example' ),
+        'chant_variants_meta_box_callback',
         'gc_chant',
         'normal',
         'default'

46449f4ea91590956ca95854f2c2600708d63362
nwshane <django09@gmail.com>
2014-08-07 13:15:17 +0400

    Added a second test for feast-day-service into test_save_all_meta.

--- wordpress/wp-content/plugins/gc-custom-post-types/phpunit-tests/tests/test-chant-post.php
+++ wordpress/wp-content/plugins/gc-custom-post-types/phpunit-tests/tests/test-chant-post.php
@@ -2,32 +2,42 @@
 
 class ChantPost extends WP_UnitTestCase {
 
-    function test_save_georgian_text() {
-        $message = 'Tests gc_save_all_meta function.';
-        $input = 'ქართული ტექსტი';
-        $expected = $input;
-
-        // Create new user and set to current user
+    function test_save_all_meta() {
+        // Setup: Create user
         $current_user = $this->factory->user->create(array(
             'role' => 'administrator'
         ));
         wp_set_current_user( $current_user );
 
-        // Create new chant post
+        // Setup: Create user
         $chant_post_id = $this->factory->post->create(array( 'post_type' => 'gc_chant' ));
 
-        // Create nonce
-        $_POST['georgian_text_nonce'] = wp_create_nonce( 'georgian-text-action' );
+        function test_setup_POST_var( $field_id, $nonce, $action, $input ) {
+            $_POST[$nonce] = wp_create_nonce( $action );
+            $_POST[$field_id] = $input;
+        }
 
-        // Make $input the value of georgian-text post variable
-        $_POST['georgian-text'] = $input;
+        $inputGeorgianText = 'ქართული ტექსტი';
+        test_setup_POST_var( 'georgian-text', 'georgian_text_nonce', 'georgian-text-action', $inputGeorgianText );
+
+        $inputFeastDayService = 'September 1st';
+        test_setup_POST_var( 'feast-day-service', 'feast_day_service_nonce', 'feast-day-service-action', $inputFeastDayService );
 
         // Save to database
         gc_save_all_meta( $chant_post_id, get_post( $chant_post_id ));
 
-        // Retrieve from database as $output
-        $output = get_post_meta( $chant_post_id, 'georgian-text', true );
-        $this->assertEquals( $expected, $output, $message );
+        // Tests
+        $this->assertEquals(
+            $inputGeorgianText,
+            get_post_meta( $chant_post_id, 'georgian-text', true ),
+            'Tests that georgian-text field is saving properly to database.'
+        );
+
+        $this->assertEquals(
+            $inputFeastDayService,
+            get_post_meta( $chant_post_id, 'feast-day-service', true ),
+            'Tests that feast-day-service is saving properly to database'
+        );
     }
 }
 

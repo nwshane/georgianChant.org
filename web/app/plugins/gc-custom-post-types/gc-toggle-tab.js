4f8e02556c781dadda2c76cc49f84a27b032c0bc
nwshane <django09@gmail.com>
2014-09-08 08:58:09 +0400

    Toggle button now hides and displays only the immediately following content tab, instead of all content tabs.

--- web/app/plugins/gc-custom-post-types/gc-toggle-tab.js
+++ web/app/plugins/gc-custom-post-types/gc-toggle-tab.js
@@ -1,14 +1,17 @@
 // The following line allows Chrome Developer Tools to detect this file in its Sources section:
 //# sourceURL=gc-toggle-tab.js
 
+
+
 function toggle_visibility() {
-    var content_tab = jQuery( 'li.single-chant-variant div.content' );
+    var target = event.target;
+    var content_tab = jQuery( target ).parent().next( 'div.content' );
     content_tab.toggleClass( 'closedTab' );
 
-    var toggle_button = jQuery('span.toggle-button');
+    var toggle_button = jQuery( target );
     if ( content_tab.hasClass( 'closedTab' )) {
-        toggle_button.html('&#x025B8;').text();
+        toggle_button.html( '&#x025B8;' ).text();
     } else {
-        toggle_button.html('&#x025BE;').text();
+        toggle_button.html( '&#x025BE;' ).text();
     }
 }

6fa6894a9c0cc4ad33f18edde5be647953a91ef5
nwshane <django09@gmail.com>
2014-09-09 15:42:09 +0400

    Deleted everything in preparation for transfer to Drupal CMS.

--- web/app/plugins/gc-custom-post-types/recording-post-type/remove-recording-file.js
+++
@@ -1,7 +0,0 @@
-// The following line allows Chrome Developer Tools to detect this file in its Sources section:
-//# sourceURL=remove-recording-file.js
-
-function remove_recording() {
-    jQuery( "#recording-file-url" ).val( "" );
-    jQuery( "#remove-recording" ).append( "<span> - Click \"Update\" to confirm deletion</span>" );
-}
